2eaa6a65465b9c54b96f29d6daddae4490aa666a
tsabitasyaa <tsasya16@gmail.com>
2025-11-20 10:35:09 +0700

    Update global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
-module.exports = {
+
+/** @type {import('tailwindcss').Config} */module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#2F4156",
+        teal: "#567C8D",
+        skyblue: "#C8D9E6",
+        beige: "#F5EFEB",
+        white: "#FFFFFF",
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+          "100%": { transform: "translate(0px, 0px) scale(1)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
+
