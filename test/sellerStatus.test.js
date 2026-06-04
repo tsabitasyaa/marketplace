454d479ed81ac9f8c0252db524cecf5b9747cbab
tsabitasyaa <tsasya16@gmail.com>
2026-06-04 23:55:12 +0700

    Add unit test for calculateSellerStatusData function

---
+++ test/sellerStatus.test.js
@@ -0,0 +1,16 @@
+// test/sellerStatus.test.js
+
+// Import fungsi yang akan diuji
+const { calculateSellerStatusData } = require('../src/app/utils/sellerStatus');
+
+function testDUPLUNIT05() {
+  const totalSellers = 120;
+  const activeSellers = 70;
+  const inactiveSellers = 50;
+
+  const result = calculateSellerStatusData(activeSellers, inactiveSellers, totalSellers);
+
+  console.log(`   ${JSON.stringify(result, null, 2)}`);
+}
+
+testDUPLUNIT05();
