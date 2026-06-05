bad2271c2501bfb85ce2e0b7eb72530db8a99f8b
tsabitasyaa <tsasya16@gmail.com>
2026-06-05 09:38:04 +0700

    update

--- test/dashboardStats.test.js
+++ test/dashboardStats.test.js
@@ -1,16 +1,16 @@
-// // test/dashboardStats.test.js
+// test/dashboardStats.test.js
 
-// const { calculateDashboardStats } = require('../src/app/utils/dashboardStats');
+const { calculateDashboardStats } = require('../src/app/utils/dashboardStats');
 
-// function testDUPLUNIT06() {
-//   const inputProducts = [
-//     { price: 1000, sold: 2, rating: 4 },
-//     { price: 2000, sold: 1, rating: 5 }
-//   ];
+function testDUPLUNIT06() {
+  const inputProducts = [
+    { price: 1000, sold: 2, rating: 4 },
+    { price: 2000, sold: 1, rating: 5 }
+  ];
 
-//   const result = calculateDashboardStats(inputProducts);
+  const result = calculateDashboardStats(inputProducts);
 
-//   console.log(`   ${JSON.stringify(result, null, 2)}`);
-// }
+  console.log(`   ${JSON.stringify(result, null, 2)}`);
+}
 
-// testDUPLUNIT06();
+testDUPLUNIT06();
